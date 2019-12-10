c74e7cdb6acab93bb84430d1ba2077e482dd7434
bikal1000 <bikal1000.bs@gmail.com>
2019-12-10 09:40:06 +0545

    backend completed

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 //app.use(express.urlencoded()); //Parse URL-encoded bodies
 
 //DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 //Connect to MongoDB
 mongoose
@@ -25,19 +25,19 @@ mongoose
 		useNewUrlParser: true,
 		useCreateIndex: true
 	})
-	.then(() => console.log("MongoDB Connected"))
+	.then(() => console.log('MongoDB Connected'))
 	.catch(err => console.log(err));
 
 //Passport middleware
 app.use(passport.initialize());
 
 //Passport Config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 //Use Routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
