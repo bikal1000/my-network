b06b7e4dea3576268b7a3e74e286a2e51fcd2f6e
bikal1000 <bikal1000.bs@gmail.com>
2019-12-12 20:39:41 +0545

    beginning of react

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+
+function App() {
+	return (
+		<div className="App">
+			<Navbar />
+			<Landing />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
