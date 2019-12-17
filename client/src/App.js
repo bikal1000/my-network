33f1ba9056fd70a8aa0695a9c506efeff3b5c672
bikal1000 <bikal1000.bs@gmail.com>
2019-12-17 21:34:46 +0545

    starting redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { provider } from 'react-redux';
+import store from './store';
+
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Resister';
+import Login from './components/auth/Login';
+
+import './App.css';
 
 function App() {
 	return (
-		<div className="App">
-			<Navbar />
-			<Landing />
-			<Footer />
-		</div>
+		<provider store={store}>
+			<Router>
+				<div className="App">
+					<Navbar />
+					<Route exact path="/" component={Landing} />
+					<div className="container">
+						<Route exact path="/register" component={Register} />
+					</div>
+					<div className="container">
+						<Route exact path="/login" component={Login} />
+					</div>
+					<Footer />
+				</div>
+			</Router>
+		</provider>
 	);
 }
 
